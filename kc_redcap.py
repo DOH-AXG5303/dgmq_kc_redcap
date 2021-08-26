87607c97f404979643e73d0afb967fd7e5ebdbfc
Alexey Gilman <Alexey.Gilman@doh.wa.gov>
2021-08-26 16:38:43 -0700

    added function remove_if_present_in_records(), cleaned up __name__ == __main__ triggers

--- kc_redcap.py
+++ kc_redcap.py
@@ -103,7 +103,7 @@ def import_data():
     return df_dict
 
 
-def infectious_check(df):
+def infectious_check(df_input):
     """
     Keep the rows of data where ANY of the departure dates fall between -2 days before test to +10 days after test
     
@@ -112,6 +112,7 @@ def infectious_check(df):
     return:
         df: pandas Dataframe
     """
+    df = df_input.copy()
 
     date_departure = [
         'Date of departure',
@@ -136,10 +137,74 @@ def infectious_check(df):
     
     return df
 
+def relevance_check(df_input):
+    """
+    keep data where any departures happened between now and 14 days ago
+    
+    args:
+        df_input: pandas dataframe
+    return:
+        pandas dataframe
+    """
+    df = df_input.copy()
+    
+    date_departure = [
+    'Date of departure',
+    'Date of departure.1',
+    'Date of departure.2',   
+    'Date of departure.3',
+    'Date of departure.4',
+    'Date of departure.5',
+    'Date of departure.6',]
+    
+    
+    days14 = timedelta(days=14)
+    now = datetime.today()
+    
+    #bool index for data where departure date is within the last 14 days
+    condition = df[date_departure].gt(now-days14).any(axis = 1)
+    
+    df = df[condition].copy()
+    
+    return df
+
+def remove_if_present_in_records(df_travel, df_records):
+    """
+    Compare travel dataframe against records dataframe. Remove any rows from df_travel, if present
+    in df_records.
+
+    args:
+        df_travel: dataframe (travel data)
+        df_records: dataframe (records that were already sent to CDC)
+    return: dataframe
+    """
+    
+    df_records = df_records.copy()
+    df = df_travel.copy()
+
+    # drop duplicate event IDs in Records data. Prep for logical comparison
+    df_records.drop_duplicates(subset="WDRS Event ID (Person) (Person)", inplace=True)
+
+    # prepare index of two dataframes, set identified (Event ID) columns as index
+    travel_index = df.set_index(keys="Record_ID").index
+    records_index = df_records.set_index(keys="WDRS Event ID (Person) (Person)").index
+
+    # mask index values, keep True only if NOT present in records dataframe event ID
+    mask = ~travel_index.isin(records_index)
+
+    # resulting rows where event ID is NOT present in Records.xlsx master file
+    df = df.loc[mask].copy()
+
+    return df
+
 if (__name__=="__main__"):
 
     df_dict = import_data()
     
-    df_travel = infectious_check(df_dict["Travel"])
+    df_travel = (df_dict["Travel"]
+                 .pipe(infectious_check)
+                 .pipe(relevance_check))
+    
+    df_travel = remove_if_present_in_records(df_travel, df_dict["Records"])
     
     df_travel.to_csv("./Redcap_Result.csv", index=False)
